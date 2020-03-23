76cfeb9fa1a777e85c1fdbb493f89bc69cabfab8
Josh Hoeg <josh.hoeg@du.edu>
2020-03-23 02:06:46 -0600

    second commit

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,48 @@
+import app from 'firebase/app';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyBOzLGdlS1boV5SegE5MnhRJLP2Is_lw0E",
+    authDomain: "thehelpboard.firebaseapp.com",
+    databaseURL: "https://thehelpboard.firebaseio.com",
+    projectId: "thehelpboard",
+    storageBucket: "thehelpboard.appspot.com",
+    messagingSenderId: "450773720854",
+    appId: "1:450773720854:web:3e03bf77ec78dac36cfa37",
+    measurementId: "G-24XDZKHTT3"
+  };
+
+//   const devConfig = {
+//     apiKey: process.env.REACT_APP_DEV_API_KEY,
+//     authDomain: process.env.REACT_APP_DEV_AUTH_DOMAIN,
+//     databaseURL: process.env.REACT_APP_DEV_DATABASE_URL,
+//     projectId: process.env.REACT_APP_DEV_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_DEV_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_DEV_MESSAGING_SENDER_ID,
+//   };
+//   const config =
+//     process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
+
+class Firebase {
+    constructor() {
+      app.initializeApp(config);
+
+      this.auth = app.auth();
+    }
+
+    // *** Auth API ***
+    doCreateUserWithEmailAndPassword = (email, password) => 
+        this.auth.createUserWithEmailAndPassword(email, password);
+
+    doSignInWithEmailAndPassword = (email, password) => 
+        this.auth.signInWithEmailAndPassword(email, password);
+
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+    doPasswordUpdate = password => 
+        this.auth.currentUser.updatePassword(password);
+        
+  }
+  export default Firebase;
