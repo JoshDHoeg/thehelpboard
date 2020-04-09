2178175fb8233d8bbf2fd8d3b971c10b23285280
Josh Hoeg <josh.hoeg@du.edu>
2020-04-09 03:10:28 -0600

    finished first tutorial

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
     apiKey: "AIzaSyBOzLGdlS1boV5SegE5MnhRJLP2Is_lw0E",
@@ -28,6 +29,7 @@ class Firebase {
       app.initializeApp(config);
 
       this.auth = app.auth();
+      this.db = app.database();
     }
 
     // *** Auth API ***
@@ -43,6 +45,11 @@ class Firebase {
 
     doPasswordUpdate = password => 
         this.auth.currentUser.updatePassword(password);
-        
+
+    // *** User API ***
+    user = uid => this.db.ref(`users/${uid}`);
+  
+    users = () => this.db.ref('users');
+
   }
   export default Firebase;
