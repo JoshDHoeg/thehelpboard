a3a79c0a5d12d8750eb6e9416e74aac999d4d3ce
Josh Hoeg <josh.hoeg@du.edu>
2020-04-11 12:48:30 -0600

    added bootstrap

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { Navbar, Nav } from 'react-bootstrap';
 
 import { AuthUserContext } from '../Session';
 import SignOutButton from '../SignOut';
@@ -18,34 +18,32 @@ const Navigation = () => (
   </AuthUserContext.Consumer>
 );
 const NavigationAuth = ({ authUser }) => (
-  <ul>
-    <li>
-      <Link to={ROUTES.LANDING}>Landing</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.HOME}>Home</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.ACCOUNT}>Account</Link>
-    </li>
-    {!!authUser.roles[ROLES.ADMIN] && (
-      <li>
-        <Link to={ROUTES.ADMIN}>Admin</Link>
-      </li>
-    )}
-    <li>
-      <SignOutButton />
-    </li>
-  </ul>
+  <Navbar bg="light" expand="lg">
+    <Navbar.Brand href="#home">Airia Helpboard</Navbar.Brand>
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link><Link to={ROUTES.LANDING}>Landing</Link></Nav.Link>
+        <Nav.Link><Link to={ROUTES.HOME}>Home</Link></Nav.Link>
+        <Nav.Link><Link to={ROUTES.ACCOUNT}>Account</Link></Nav.Link>
+        {!!authUser.roles[ROLES.ADMIN] && (
+        <li>
+          <Nav.Link href="#link"><Link to={ROUTES.ADMIN}>Admin</Link></Nav.Link>
+        </li>
+        )}
+        <Nav.Link href="#link"><SignOutButton /></Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
 );
 const NavigationNonAuth = () => (
-  <ul>
-    <li>
-      <Link to={ROUTES.LANDING}>Landing</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-    </li>
-  </ul>
+  <Navbar bg="light" expand="lg">
+    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="#home"><Link to={ROUTES.LANDING}>Landing</Link></Nav.Link>
+        <Nav.Link href="#link"><Link to={ROUTES.SIGN_IN}>Sign In</Link></Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
 );
 export default Navigation;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import App from './components/App';
 import Firebase, { FirebaseContext } from './components/Firebase';
+import './assets/scss/style.scss';
 ReactDOM.render(
   <FirebaseContext.Provider value={new Firebase()}>
     <App />
